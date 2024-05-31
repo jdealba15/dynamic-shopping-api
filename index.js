d629a7803a1fb8a70ca739277829cb702069604f
Jonathan De Alba <jdealba15@yahoo.com>
2024-05-31 13:57:02 -0700

    first commit

--- index.js
+++ index.js
@@ -14,40 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
 
-// app.post('/', (req, res) => {
-//     const data = req.body;
-
-//     console.log('Received Data:', data);
-//     res.status(200).json({ message: 'Data received successfully' });
-
-//     async function main() {
-//     const completion = await openai.chat.completions.create({
-//             messages: [{ role: "system", content: "You are a helpful assistant."
-//             }],
-//             model: "gpt-3.5-turbo",
-//             });
-//             console.log(completion.choices[0]);
-//             }
-//             main();
-// });
-
-app.post('/', (req, res) => {
-    const groceryItems = req.body.join(", ");
-
-    console.log('Received Data:', groceryItems);
-    res.status(200).json({ message: 'Data received successfully' });
-
-    async function main() {
-    const completion = await openai.chat.completions.create({
-            messages: [{ role: "system", content: prompt(groceryItems)
-            }],
-            model: "gpt-3.5-turbo",
-            });
-            console.log(completion.choices[0]);
-            }
-            main();
-});
-
 const prompt = (groceryItems) =>`
     Given a list of grocery items, categorize each item into categories like,
     but not limited to: Dairy, Fruits & Vegetables,
@@ -79,7 +45,22 @@ const prompt = (groceryItems) =>`
     characters.
     `
 
+app.post('/', (req, res) => {
+    const groceryItems = req.body.join(", ");
 
+    console.log('Received Data:', groceryItems);
+    res.status(200).json({ message: 'Data received successfully' });
+
+    async function main() {
+    const completion = await openai.chat.completions.create({
+            messages: [{ role: "system", content: prompt(groceryItems)
+            }],
+            model: "gpt-3.5-turbo",
+            });
+            console.log(completion.choices[0]);
+            }
+            main();
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
